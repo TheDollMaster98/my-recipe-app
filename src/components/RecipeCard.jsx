8dc0d4a4178089addc894c268b82e0c62bedef7d
TheDollMaster98 <lorisdalsanto@hotmail.it>
2024-10-06 18:42:00 +0200

    redesing card

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const RecipeCard = ({ meal }) => {
   return (
-    <Link to={`/recipe/${meal.idMeal}`} className="block text-black no-underline">
-      <div className="max-w-xs p-4 bg-white rounded shadow">
-        <img src={meal.strMealThumb} alt={meal.strMeal} className="object-cover w-full h-24 rounded" />
-        <div className="flex justify-between mt-2">
-          <h3 className="text-sm font-bold">{meal.strMeal}</h3>
-        </div>
-        <p className="text-xs text-gray-600">Categoria: {meal.strCategory}</p>
+    <Link
+      to={`/recipe/${meal.idMeal}`}
+      className="block overflow-hidden text-black no-underline bg-white rounded shadow-lg"
+    >
+      <img
+        src={meal.strMealThumb}
+        alt={meal.strMeal}
+        className="object-cover w-full h-48"
+      />
+      <div className="p-4">
+        {/* truncate per gestire il testo lungo */}
+        <h3 className="text-lg font-bold truncate">{meal.strMeal}</h3>{" "}
+        <p className="text-gray-600">Categoria: {meal.strCategory}</p>
       </div>
     </Link>
   );
