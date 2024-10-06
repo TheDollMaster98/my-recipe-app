c97b2d90365c57f60aedd0a6369ef05a8dc9cd94
TheDollMaster98 <lorisdalsanto@hotmail.it>
2024-10-06 18:47:29 +0200

    cambio grid

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,34 +1,36 @@
-import React, { useEffect, useState } from 'react';
-import { getRandomMeal } from '../api/mealApi';
-import RecipeCard from '../components/RecipeCard';
-import { setSessionData, getSessionData } from '../api/session'; // Importa le funzioni
+import React, { useEffect, useState } from "react";
+import { getRandomMeal } from "../api/mealApi";
+import RecipeCard from "../components/RecipeCard";
+import { setSessionData, getSessionData } from "../api/session"; // Importa le funzioni
 
 const HomePage = () => {
   const [allMeals, setAllMeals] = useState([]);
-  const [numMeals, setNumMeals] = useState(12);
+  const [numMeals, setNumMeals] = useState(25);
 
   // UseEffect esegue la funzione fetchInitialMeals solo al montaggio del componente
   useEffect(() => {
     const fetchInitialMeals = async () => {
-      const cachedMeals = getSessionData('allMeals'); 
-      
+      const cachedMeals = getSessionData("allMeals");
+
       if (cachedMeals) {
         setAllMeals(cachedMeals);
       } else {
-        const mealPromises = Array.from({ length: numMeals }, () => getRandomMeal());
+        const mealPromises = Array.from({ length: numMeals }, () =>
+          getRandomMeal()
+        );
         const results = await Promise.all(mealPromises);
         setAllMeals(results);
-        setSessionData('allMeals', results); // Usa la funzione per memorizzare i dati
+        setSessionData("allMeals", results); // Usa la funzione per memorizzare i dati
       }
     };
 
     fetchInitialMeals();
-  }, []); // 
+  }, []); //
 
   useEffect(() => {
     const addRandomMeal = async () => {
       const newMeal = await getRandomMeal();
-      setAllMeals(prevMeals => [...prevMeals, newMeal]);
+      setAllMeals((prevMeals) => [...prevMeals, newMeal]);
     };
 
     if (allMeals.length < numMeals) {
@@ -37,11 +39,13 @@ const HomePage = () => {
   }, [numMeals, allMeals.length]); // Esegui quando numMeals cambia
 
   return (
-    <div className="min-h-screen bg-black-1">      
-      <div className="container p-4 mx-auto mt-8">
+    <div className="min-h-screen bg-black-1">
+      <div className="p-4 mx-auto mt-8 ">
         <h2 className="text-2xl font-bold text-center">Ricette Popolari</h2>
         <div className="flex justify-center mt-4">
-          <label htmlFor="numMeals" className="mr-2">Numero di ricette da mostrare:</label>
+          <label htmlFor="numMeals" className="mr-2">
+            Numero di ricette da mostrare:
+          </label>
           <input
             id="numMeals"
             type="number"
@@ -51,13 +55,16 @@ const HomePage = () => {
             className="p-2 border border-gray-300 rounded"
           />
         </div>
-        <div className="grid grid-cols-4 gap-4 mt-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-          {allMeals.slice(0, numMeals).map(meal => (
+        <div className="grid grid-cols-5 mt-4 gap-7 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5">
+          {" "}
+          {allMeals.slice(0, numMeals).map((meal) => (
             <RecipeCard key={meal.idMeal} meal={meal} />
           ))}
         </div>
         <div className="mt-8 text-center">
-          <h2 className="text-2xl font-bold">Iscriviti alla nostra Newsletter</h2>
+          <h2 className="text-2xl font-bold">
+            Iscriviti alla nostra Newsletter
+          </h2>
           <input
             type="email"
             placeholder="Inserisci la tua email"
