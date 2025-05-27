b677f179668581a8b22ce72698a04dbc2b270cbe
Loris <78973637+TheDollMaster98@users.noreply.github.com>
2025-05-27 15:12:57 +0200

    fix recipe icon

--- src/pages/SavedRecipesPage.jsx
+++ src/pages/SavedRecipesPage.jsx
@@ -16,8 +16,14 @@ const SavedRecipesPage = () => {
 
   return (
     <div className="min-h-screen p-6 bg-gray-100">
-      <h2 className="mb-6 text-3xl font-bold text-center text-black">Il Mio Ricettario 📖</h2>
-
+      <h2 className="mb-6 text-3xl font-bold text-center text-black">
+        <div className="flex items-center justify-center gap-2">
+          Il Mio Ricettario
+          <span className="text-blue-500 material-icons" style={{ fontSize: "2.5rem" }}>
+            menu_book
+          </span>
+        </div>
+      </h2>
       {savedRecipes.length > 0 ? (
         <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {savedRecipes.map((meal) => (
